6f3dab67c47bd75cb0bc09246382ff7fa05c0392
Quangbao309 <nguyenbao12405@gmail.com>
2025-01-15 19:34:16 +0700

    Delete 3d-periodic-table-main directory

--- 3d-periodic-table-main/src/atomData.ts
+++
@@ -1,5 +0,0 @@
-import jsonData from "./data/data.json";
-import AtomInfo from "./types/AtomInfo";
-
-const data: AtomInfo[] = jsonData as AtomInfo[];
-export default data;
--- 3d-periodic-table-main/src/components/Control/Style.ts
+++
@@ -1,8 +0,0 @@
-type Style = {
-  color: string;
-  opacity?: number;
-  metalness?: number;
-  roughness?: number;
-};
-
-export default Style;
